d68ebae4d8a3709ef6644a150152310f9813b359
Nikolos Hurrinus <potatomasterme@gmail.com>
2026-08-12 02:16:07 -0400

    initial setup complete

---
+++ src/main/java/nikoloshurrinus/interviewproject/FirestoreContext.java
@@ -0,0 +1,64 @@
+package nikoloshurrinus.interviewproject;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/*
+this class utilizes the fire store's provided code to access and utilize the fire store database and authentication
+features
+*/
+
+public class FirestoreContext {
+
+    public Firestore firebase() {
+        try {
+
+            FileInputStream serviceAccount =
+                    new FileInputStream("src/main/resources/nikoloshurrinus/interviewproject/key.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.exit(1);
+        }
+        return FirestoreClient.getFirestore();
+    }
+
+    public static Firestore getDB() {
+
+        try {
+
+            if (FirebaseApp.getApps().isEmpty()) {
+
+                InputStream serviceAccount =
+                        FirestoreContext.class.getResourceAsStream(
+                                "src/main/resources/nikoloshurrinus/interviewproject/key.json");
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return FirestoreClient.getFirestore();
+    }
+
+
+}
---
+++ src/main/java/nikoloshurrinus/interviewproject/Launcher.java
@@ -0,0 +1,9 @@
+package nikoloshurrinus.interviewproject;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(InventoryTracker.class, args);
+    }
+}
